82960fe2128ace8f108e9b97c7255a64f6623ad0
studentqa2021 <sarowerhome@gmail.com>
2022-01-27 18:28:55 -0500

    new project

--- src/main/java/com/generic/practice/BaseLoginPractice.java
+++ src/main/java/com/generic/practice/BaseLoginPractice.java
@@ -2,24 +2,32 @@ package com.generic.practice;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseLoginPractice {
-     public void getlogin(){
+     
+	public void getlogin(){
     	
    System.setProperty("webdriver.chrome.driver","/Users/waris/Downloads/chromedriver");
     	 
     WebDriver driver = new ChromeDriver();
 	
-   driver.get("https://https://demo.guru99.com/test/newtours/");
-	
-  driver.findElement(By.xpath(null));
+   driver.get("https://demo.guru99.com/test/newtours/index.php");
+   
+   driver.manage().window().maximize();//this code for  bigger page. 
+  
+  WebElement user  =driver.findElement(By.xpath(" //*[@name='userName']"));
      
+     user.sendKeys("ali");
      
+WebElement password  =driver.findElement(By.xpath(" //*[@name='password']"));
      
+     password.sendKeys("ali12345");
      
+WebElement submitbutton  =driver.findElement(By.xpath(" //*[@name='submit']"));
      
-     
+     submitbutton.click();
      
      }
 
--- src/test/java/com/smoke/practice/SmokeTestRun.java
+++ src/test/java/com/smoke/practice/SmokeTestRun.java
@@ -11,7 +11,6 @@ public static void main(String[] args) {
 	obj.getlogin();
 
 
-
 }
    
 }
